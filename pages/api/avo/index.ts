8caf74b16d37528cb203fd575cad707c7c9d7484
Christian Montoya <chrimonlo@gmail.com>
2023-05-16 23:26:03 -0500

    Obtain all data

--- pages/api/avo/index.ts
+++ pages/api/avo/index.ts
@@ -3,9 +3,13 @@ import DB from './../../../database/db';
 const allAvos = async (request: IncomingMessage, response: ServerResponse) => {
   const db = new DB();
   const allEntries = await db.getAll();
+  const quantity = allEntries.length;
   response.statusCode = 200;
   response.setHeader('Content-type','application/json')
-  response.end(JSON.stringify({ data: allEntries }))
+  response.end(JSON.stringify({
+    quantity,
+    data: allEntries
+  }))
 }
 
 export default allAvos;
