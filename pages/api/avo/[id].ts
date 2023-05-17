fa1dca69b9f4ac17b26d818413f9eead0241c13f
Christian Montoya <chrimonlo@gmail.com>
2023-05-16 23:41:00 -0500

    Adjustments in the api

--- pages/api/avo/[id].ts
+++ pages/api/avo/[id].ts
@@ -4,11 +4,8 @@ const oneAvo = async (request: NextApiRequest, response: NextApiResponse) => {
   const db = new DB();
   const id = request.query.id;
   const entry = await db.getById(id as string);
-  response.statusCode = 200;
   response.setHeader('Content-type','application/json')
-  response.end(JSON.stringify({
-    data: entry
-  }))
+  response.status(200).json({data: entry})
 }
 
 export default oneAvo;
--- pages/api/avo/index.ts
+++ pages/api/avo/index.ts
@@ -1,15 +1,16 @@
-import { IncomingMessage, ServerResponse } from "http";
 import DB from './../../../database/db';
-const allAvos = async (request: IncomingMessage, response: ServerResponse) => {
+import { NextApiRequest, NextApiResponse } from "next";
+const allAvos = async (request: NextApiRequest, response: NextApiResponse) => {
   const db = new DB();
   const allEntries = await db.getAll();
   const quantity = allEntries.length;
-  response.statusCode = 200;
   response.setHeader('Content-type','application/json')
-  response.end(JSON.stringify({
-    quantity,
-    data: allEntries
-  }))
+  response.status(200).json(
+    {
+      quantity,
+      data: allEntries
+    }
+  )
 }
 
 export default allAvos;
