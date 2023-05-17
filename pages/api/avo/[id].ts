ffcad442a0ac55c59e82f35a5451ab0abb46357d
Christian Montoya <chrimonlo@gmail.com>
2023-05-16 23:32:38 -0500

    Get one item

---
+++ pages/api/avo/[id].ts
@@ -0,0 +1,14 @@
+import DB from './../../../database/db';
+import { NextApiRequest, NextApiResponse } from "next";
+const allAvos = async (request: NextApiRequest, response: NextApiResponse) => {
+  const db = new DB();
+  const id = request.query.id;
+  const entry = await db.getById(id as string);
+  response.statusCode = 200;
+  response.setHeader('Content-type','application/json')
+  response.end(JSON.stringify({
+    data: entry
+  }))
+}
+
+export default allAvos;
