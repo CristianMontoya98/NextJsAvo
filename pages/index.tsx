d4c12d5a2e93e21252f2af0f8630b3a5f9716eae
Christian Montoya <chrimonlo@gmail.com>
2023-06-26 22:30:15 -0500

    Implementing header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar/Navbar'
+import Header from '../components/Header/Header'
 const HomePage = () => {
   const [products, setProducts] = useState<TProduct[]>([])
   useEffect(() => {
     window
       .fetch('/api/avo')
       .then((response) => response.json())
-      .then(({ data, quantity }) => {
+      .then(({ data }: TAPIAvoResponse) => {
         setProducts(data)
       })
   }, [])
   /*  */
   return (
     <div>
+      <Header />
       <div>Products</div>
       {products.map((avo, index) => (
         <h2 key={index}>{avo.name}</h2>
