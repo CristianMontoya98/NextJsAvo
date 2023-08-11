cc5edecd4fff3a255ca51d48e41a322ee443252c
Christian Montoya <chrimonlo@gmail.com>
2023-08-10 20:03:06 -0500

    Server side rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,18 @@ import React, { useState, useEffect } from 'react'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
+import fetch from 'isomorphic-unfetch'
+export const getServerSideProps = async () => {
+  const response = await fetch('https://platzi-avo.vercel.app/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
+  return {
+    props: {
+      productList,
+    },
+  }
+}
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
-
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
-
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
   return (
     <Layout>
       <KawaiiHeader />
